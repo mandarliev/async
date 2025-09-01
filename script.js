2edb2ff16d22ababf76c78905343d332b3543233
Dimitar <dimitar.mandarliev@gmail.com>
2025-09-01 14:52:34 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+// NEW COUNTRIES API URL (use instead of the URL shown in videos):
+// https://countries-api-836d.onrender.com/countries/
+
+// NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
+// https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
+
+///////////////////////////////////////
